02c20c19c4b1c974697d2b94f3eda733cfe06ee6
Kutov Danil <kutov.danil@yandex.ru>
2026-01-30 15:30:01 +0300

    add corrections

--- settings.py
+++ settings.py
@@ -1,10 +1,14 @@
 """Конфигурация создания приложения."""
 
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 class Config(object):
     """Задание настроект для Flask."""
 
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_value')
